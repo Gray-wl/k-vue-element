50f4648dd493dd1afd4fbb460849a2a5cdbfa707
wanglei4 <dayee@306>
2020-11-03 15:35:05 +0800

    update k-table.spec.js

--- tests/unit/k-table.spec.js
+++ tests/unit/k-table.spec.js
@@ -1,4 +1,5 @@
-import {shallowMount} from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
+import KTable from '@/components/table/KTable.vue'
 import KTableColumn from '@/components/table/KTableColumn.vue'
 
 const getTestData = function () {
@@ -25,52 +26,63 @@ const getTestData = function () {
   }];
 };
 
-describe('KTableColumn.vue', () => {
-  it('数据显示', () => {
-    const propsData = {
-      prop: 'date',
-      label: '日期',
-      sortable: false,
-    }
-    const wrapper = shallowMount(KTableColumn, {
-      inject: {table: {default: {}}},
-      propsData: propsData,
-      computed: {
-        sort() {
-          return {};
-        },
-        data() {
-          return getTestData();
-        }
+describe('KTable', () => {
+  describe('数据显示', () => {
+    const defaultSort = {
+      prop: 'date', order: 'descending'
+    };
+    const wrapper = shallowMount(KTable, {
+      slots: {
+        default: '<k-table-column  prop="date" label="日期" sortable></k-table-column>'
+      },
+      stubs: {
+        'k-table-column': KTableColumn
+      },
+      propsData: {
+        data: getTestData(),
+        defaultSort: defaultSort
       }
     })
-    const h3 = wrapper.find('h3');
-    const list = wrapper.findAll('.k-table-column-list');
-    // toMatch
-    expect(h3.text()).toBe(propsData.label)
-    expect(list.length).toBe(getTestData().length)
-  })
 
-  it('数据变动', () => {
-    const propsData = {
-      prop: 'date',
-      label: '日期',
-      sortable: true,
-    }
-    const wrapper = shallowMount(KTableColumn, {
-      inject: {table: {default: {}}},
-      propsData: propsData,
-      computed: {
-        sort() {
-          return {};
-        },
-        data() {
-          return getTestData();
+    it('KTable列表数据', () => {
+      const tableData = wrapper.vm.tableData;
+      expect(JSON.stringify(tableData)).toBe(JSON.stringify(getTestData().sort((a, b) => {
+        if (a[defaultSort.prop] < b[defaultSort.prop]) {
+          return 1
+        } else if (a[defaultSort.prop] > b[defaultSort.prop]) {
+          return -1
+        } else {
+          return 0
         }
-      }
+      })));
     })
-    const h3 = wrapper.find('h3');
-    const sortBtn = h3.find('.caret-wrapper');
-    expect(sortBtn.isVisible()).toBe(true);
-  });
-})
+
+    it('KTableColumnBox组件显示', async () => {
+      const KTableColumnBox = wrapper.findComponent(KTableColumn);
+      const vm = KTableColumnBox.vm;
+      // KTableColumn组件是否显示
+      expect(KTableColumnBox.exists()).toBe(true);
+
+      // label显示是否正常
+      const h3 = KTableColumnBox.find('h3');
+      const label = KTableColumnBox.props().label;
+      expect(h3.text()).toBe(label);
+
+      // 排序按钮是否显示
+      const prop = KTableColumnBox.props().prop;
+      const sortBtn = h3.find('.caret-wrapper');
+      expect(sortBtn.isVisible()).toBe(!!KTableColumnBox.props().sortable);
+      expect(KTableColumnBox.classes(vm.sort.order)).toBe(true);
+
+      // 触发排序按钮
+      await h3.trigger('click');
+      expect(vm.sort.prop).toBe(prop);
+      const tableData = vm.data;
+      expect(JSON.stringify(tableData)).toBe(JSON.stringify(getTestData()));
+
+      //列表数据是否展示 长度是否一致
+      const list = KTableColumnBox.findAll('.k-table-column-list');
+      expect(list.length).toBe(getTestData().length);
+    })
+  })
+});
